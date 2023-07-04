63730253a13b6e7fc6e746da8a63a92708b17fbc
Ed-Neema <98660147+Ed-Neema@users.noreply.github.com>
2023-07-04 12:59:38 +0300

    simple express server

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http  = require('http');
 const server = http.createServer((req,res)=>{
     res.end("Hello from the server")
 });
+
 //port,address, callback function executed when server connected successfully
 server.listen(8000,"localhost",()=>{
     console.log("Server started successfully");    
-})
+});
---
+++ one.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app =  express();
+
+// route
+app.get("/", (req,res)=>{
+    res.send("hello from express server")
+})
+
+//port, callback function executed when server connected successfully
+app.listen(8000,()=>{
+    console.log("Server started successfully")
+})
